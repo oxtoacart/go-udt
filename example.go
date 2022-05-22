01e3ac36cbe2a46b7f9e21e661ba06818bdc1d0b
Vadym Vikulin <vadym.vikulin@gmail.com>
2022-05-22 20:46:53 +0300

    Update example.go

--- example.go
+++ example.go
@@ -3,29 +3,23 @@ package main
 import (
 	"github.com/oxtoacart/go-udt/udt"
 	"log"
-	"net"
 	"time"
 )
 
 func main() {
-	if addr, err := net.ResolveUDPAddr("udp", "localhost:47008"); err != nil {
-		log.Fatalf("Unable to resolve address: %s", err)
-	} else {
-		go server(addr)
-		time.Sleep(200 * time.Millisecond)
-		go client(addr)
-
-		time.Sleep(50 * time.Second)
-	}
+	go server("localhost:47008")
+	time.Sleep(200 * time.Millisecond)
+	go client("localhost:47008")
+	time.Sleep(50 * time.Second)
 }
 
-func server(addr *net.UDPAddr) {
+func server(addr string) {
 	if _, err := udt.ListenUDT("udp", addr); err != nil {
 		log.Fatalf("Unable to listen: %s", err)
 	}
 }
 
-func client(addr *net.UDPAddr) {
+func client(addr string) {
 	if _, err := udt.DialUDT("udp", nil, addr); err != nil {
 		log.Fatalf("Unable to dial: %s", err)
 	}
